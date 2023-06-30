7618c57227e2ecce8150c4185e2c290bae0ee5cd
Ryan Zillini <nyrtrading134@gmail.com>
2023-06-30 12:59:26 -0400

    completed app

--- my-repo/src/App.js
+++ my-repo/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState({ sport: "", id: "" });
+  const [sportsList, setSportsList] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setValue({ ...value, [e.target.name]: [e.target.value] });
+  };
+
+  const handleSubmit = (e) => {
+    const newSport = {
+      sport: value.sport,
+      id: Date.now(),
+    };
+    e.preventDefault();
+    setSportsList([...sportsList, newSport]);
+    setValue({ sport: "" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Favorite Sports</h1>
+      {sportsList.map((sport) => {
+        return <li key={sport.id}>{sport.sport}</li>;
+      })}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="sport"
+            value={value.sport}
+            placeholder="Enter Favorite Sport"
+            onChange={handleChange}
+          />
+          <button>Enter</button>
+        </form>
+      </div>
     </div>
   );
 }
